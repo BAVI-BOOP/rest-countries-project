1ec1ee814272864b5419cc01a6536c18823c125b
Ahmet <birceste@gmail.com>
2022-09-24 15:06:07 +0300

    bottom links removed

--- app.js
+++ app.js
@@ -97,10 +97,6 @@ const renderCountries = (data) => {
             ${Object.values(currencies)[0].symbol},
           </li>
         </ul>
-        <div class="card-body">
-          <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
-        </div>
       </div>
     `;
 };
